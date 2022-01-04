36f78fa5c8550c4011ae2d5002b22e7a10e4d693
SHAURYA75 <91466693+SHAURYA75@users.noreply.github.com>
2022-01-04 15:21:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var firstname="Ronav";
+            console.log(firstname);
+
+            var lastname="Sharma";
+            console.log(lastname);
+            
+            var fullname=firstname.concat(lastname);
+            console.log(fullname);
+
+            var lengthoffullname=fullname.length;
+            console.log(lengthoffullname);
+var a= 15;
+var b= 10;
+          var add= a+b;
+        console.log(add)
+      var sub= a-b;
+        console.log(sub);
+  var divide= a/b;
+        console.log(divide);
+ var multiply= a * b;
+        console.log(multiply);
